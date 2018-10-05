852befc48818fd9be634a7ca16052a0ec1810442
Jason Data <contact@developforjoy.com>
2018-10-05 11:04:41 +1000

    Add vanilla js toggle

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,13 +1,68 @@
-$(window).on("load resize", function(e) {
-  var menuToggle = $(".header__menu__display__button").unbind();
+var show = function (elem) {
+  var getHeight = function () {
+    elem.style.display = 'block'; // Make it visible
+    var height = elem.scrollHeight + 'px'; // Get it's height
+    elem.style.display = ''; //  Hide it again
+    return height;
+  };
 
-  menuToggle.on("click", function(e) {
-    e.preventDefault();
+  var height = getHeight();
+  elem.classList.add('is-visible'); // Make the element visible
+  elem.style.height = height; // Update the height
 
-    $(".header__top__menu").slideToggle();
-  });
+  // Once the transition is complete, remove the inline height so the content can scale responsively
+  window.setTimeout(function () {
+    elem.style.height = '';
+  }, 350);
+};
 
-  if ($(window).width() > 624) {
-    $(".header__top__menu").show();
+var hide = function (elem) {
+  // Give the element a height to change from
+  elem.style.height = elem.scrollHeight + 'px';
+
+  // Set the height back to 0
+  window.setTimeout(function () {
+    elem.style.height = '0';
+  }, 1);
+
+  // When the transition is complete, hide it
+  window.setTimeout(function () {
+    elem.classList.remove('is-visible');
+  }, 350);
+};
+
+// Toggle element visibility
+var toggle = function (elem) {
+  // If the element is visible, hide it
+  if (elem.classList.contains('is-visible')) {
+    hide(elem);
+    return;
   }
+
+  // Otherwise, show it
+  show(elem);
+};
+
+var idempotentToggle = function (elem) {
+  // if element is disabled return
+  if (elem.getAttribute("disabled") === 'true') { return; }
+
+  // set element disable
+  elem.setAttribute("disabled", true);
+
+  toggle(elem, 350);
+
+  window.setTimeout(function () {
+    // remove disabled
+    elem.removeAttribute("disabled");
+  }, 750);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  var menu = document.querySelector('.header__top__menu');
+  var button = document.querySelector('.header__menu__display__button');
+
+  button.addEventListener('click', function(e) {
+    idempotentToggle(menu);
+  });
 });
