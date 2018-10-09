9738f38105a296a5bd80c35ef0b343138c8968c4
Jason Data <contact@developforjoy.com>
2018-10-09 14:01:25 +1100

    Update config

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
+configure :build do
+  activate :relative_assets
 #   activate :minify_javascript
-# end
+end
